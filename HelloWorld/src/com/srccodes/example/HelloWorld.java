b4bd29357f57a851945d2756cd60ea21d8b0e7c7
yagya-dev <devyagya579@gmail.com>
2017-02-06 14:11:31 +0530

    just for checkin what have u done

---
+++ HelloWorld/src/com/srccodes/example/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.srccodes.example;
+
+public class HelloWorld {
+
+	/**
+	 * @param <String>
+	 * @param args
+	 */
+	public static  void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey World!");
+
+	}
+
+}
